a84a8ce5f89fbff4fbb6f51b6da124e496e85c34
hanytolba <hanytolba@gmail.com>
2023-05-20 23:04:44 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Elctric_Consumption
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            textBox1.Font = new Font("Segoe UI", 16.2F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(155, 118);
+            textBox1.MaxLength = 6;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(214, 43);
+            textBox1.TabIndex = 1;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // label1
+            // 
+            label1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(12, 46);
+            label1.Name = "label1";
+            label1.Size = new Size(92, 28);
+            label1.TabIndex = 2;
+            label1.Text = "الاستهلاك";
+            label1.TextAlign = ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.ForeColor = Color.Red;
+            label2.Location = new Point(375, 118);
+            label2.Name = "label2";
+            label2.Size = new Size(162, 25);
+            label2.TabIndex = 3;
+            label2.Text = "استهلاك عالى جدا !!";
+            label2.Visible = false;
+            // 
+            // label3
+            // 
+            label3.AllowDrop = true;
+            label3.AutoSize = true;
+            label3.CausesValidation = false;
+            label3.Location = new Point(12, 116);
+            label3.MaximumSize = new Size(150, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(120, 54);
+            label3.TabIndex = 4;
+            label3.Text = "كمية الاستهلاك بالكيلو وات";
+            label3.TextAlign = ContentAlignment.MiddleCenter;
+            label3.UseCompatibleTextRendering = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(549, 197);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            RightToLeft = RightToLeft.Yes;
+            RightToLeftLayout = true;
+            Text = "احتساب قيمة استهلاك الكهرباء الاستخدام المنزلى";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox1;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿namespace Elctric_Consumption
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            double consumption;
+            string phase = "";
+            double rate = 0;
+            double bill = 0;
+
+
+            if (double.TryParse(textBox1.Text, out rate))
+
+
+            {
+                consumption = Convert.ToDouble(textBox1.Text);
+                if (consumption > 1000)
+                {
+                    label2.Visible = true;
+                    //consumption = 1000;
+                    label1.Text = " ? ";
+                }
+                else
+                {
+                    label2.Visible = false;
+                }
+
+
+                if (consumption <= 50)
+                {
+                    phase = "الاولى";
+                    rate = 0.48;
+                    bill = (consumption * rate) + 1;
+                }
+                else if (consumption > 50 && consumption <= 100)
+                {
+                    phase = "الثانية";
+                    rate = 0.58;
+                    bill = (50 * 0.48) + ((consumption - 50) * rate) + 2;
+                }
+                else if (consumption > 100 && consumption <= 200)
+                {
+                    phase = "الثالثة";
+                    rate = 0.77;
+                    bill = consumption * rate + 6;
+                }
+                else if (consumption > 200 && consumption <= 350)
+                {
+                    phase = "الرابعة";
+                    rate = 1.06;
+                    bill = (200 * 0.77) + ((consumption - 200) * rate) + 11;
+                }
+                else if (consumption > 351 && consumption <= 650)
+                {
+                    phase = "الخامسة";//end
+                    rate = 1.28;
+                    bill = (200 * 0.77) + (150 * 1.06) + +((consumption - 350) * rate) + 15;
+                }
+                else if (consumption > 651 && consumption <= 1000)
+                {
+                    phase = "السادسة";
+                    rate = 1.28;
+                    bill = ((consumption) * rate) + 25;
+                }
+                else if (consumption > 1000)
+                {
+                    phase = "السابعة";
+                    rate = 1.45;
+                    bill = (consumption) * rate + 40;
+                }
+                bill = Math.Round(bill, 1);
+                label1.Text = " انت فى الشريحة" + phase + "  الاستهلاك  =  " + bill.ToString() + "  جنية مصرى  ";
+            }
+            else
+            {
+                label1.Text = "خطأ ادخل ارقام ";
+            }
+
+        }
+
+
+    }
+
+}
